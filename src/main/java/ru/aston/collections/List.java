93c1e19362589bdf3b20c8331d6fbf7212984752
feyour <feyour@gmail.com>
2025-02-02 16:50:07 +0300

    Update List

---
+++ src/main/java/ru/aston/collections/List.java
@@ -0,0 +1,17 @@
+package ru.aston.collections;
+
+public interface List<T> extends Iterable<T> {
+    void add(T value);
+
+    T set(int index, T newValue);
+
+    T remove(int index);
+
+    T get(int index);
+
+    void sort();
+
+    int size();
+
+    void clean();
+}
